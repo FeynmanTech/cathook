f4d06a6066057466240fa4714c6b61f9465d6a56
nullifiedcat <nullifiedcat@gmail.com>
2017-01-21 22:59:23 +0300

    Anti-Flood

--- cathook/src/netmessage.cpp
+++ cathook/src/netmessage.cpp
@@ -26,6 +26,87 @@ bool bf_write::WriteString(const char *pStr)
 	return !IsOverflowed();
 }
 
+bf_read::bf_read()
+{
+	m_pData = NULL;
+	m_nDataBytes = 0;
+	m_nDataBits = -1; // set to -1 so we overflow on any operation
+	m_iCurBit = 0;
+	m_bOverflow = false;
+	m_bAssertOnOverflow = true;
+	m_pDebugName = NULL;
+}
+
+bf_read::bf_read( const void *pData, int nBytes, int nBits )
+{
+	m_bAssertOnOverflow = true;
+	StartReading( pData, nBytes, 0, nBits );
+}
+
+bf_read::bf_read( const char *pDebugName, const void *pData, int nBytes, int nBits )
+{
+	m_bAssertOnOverflow = true;
+	m_pDebugName = pDebugName;
+	StartReading( pData, nBytes, 0, nBits );
+}
+
+void bf_read::StartReading( const void *pData, int nBytes, int iStartBit, int nBits )
+{
+	// Make sure we're dword aligned.
+	Assert(((unsigned long)pData & 3) == 0);
+
+	m_pData = (unsigned char*)pData;
+	m_nDataBytes = nBytes;
+
+	if ( nBits == -1 )
+	{
+		m_nDataBits = m_nDataBytes << 3;
+	}
+	else
+	{
+		Assert( nBits <= nBytes*8 );
+		m_nDataBits = nBits;
+	}
+
+	m_iCurBit = iStartBit;
+	m_bOverflow = false;
+}
+
+
+bool bf_read::ReadString( char *pStr, int maxLen, bool bLine, int *pOutNumChars )
+{
+	Assert( maxLen != 0 );
+
+	bool bTooSmall = false;
+	int iChar = 0;
+	while(1)
+	{
+		char val = ReadChar();
+		if ( val == 0 )
+			break;
+		else if ( bLine && val == '\n' )
+			break;
+
+		if ( iChar < (maxLen-1) )
+		{
+			pStr[iChar] = val;
+			++iChar;
+		}
+		else
+		{
+			bTooSmall = true;
+		}
+	}
+
+	// Make sure it's null-terminated.
+	Assert( iChar < maxLen );
+	pStr[iChar] = 0;
+
+	if ( pOutNumChars )
+		*pOutNumChars = iChar;
+
+	return !IsOverflowed() && !bTooSmall;
+}
 void bf_write::WriteSBitLong( int data, int numbits )
 {
 	// Do we have a valid # of bits to encode with?
